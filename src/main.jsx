71bb938a3c54233f29a7921fcdeb27c583486d58
Vera Zakharova <92325745+verazakharova111@users.noreply.github.com>
2024-10-02 14:28:24 +0200

    add dummy list

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+const TODO = ["todo1", "todo2", "todo3", "todo4"];
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>template</StrictMode>
+  <StrictMode>
+    <ul>
+      {TODO.map((todoItem, i) => (
+        <li key={i}>{todoItem}</li>
+      ))}
+    </ul>
+  </StrictMode>
 );
