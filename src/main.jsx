fd886ceac1669515e3ab0d2d2b4fd47918bda614
Vera Zakharova <92325745+verazakharova111@users.noreply.github.com>
2024-10-02 16:02:30 +0200

    work with array

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,53 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-const TODO = ["todo1", "todo2", "todo3", "todo4"];
-
+const TODO = ["todo1", "todo2", "todo3"];
+const LIST = [
+  {
+    text: "todo1",
+    id: 1,
+  },
+  {
+    text: "todo2",
+    id: 2,
+  },
+  {
+    text: "todo3",
+    id: 3,
+  },
+];
+const DATA = [
+  ["Name", "surname", "age"],
+  ["Artur", "Zakharov", "36"],
+  ["Vera", "Zakharova", "35"],
+];
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ul>
-      {TODO.map((todoItem, i) => (
-        <li key={i}>{todoItem}</li>
+      {TODO.map((el, i) => {
+        return <li key={i}> {el}</li>;
+      })}
+    </ul>
+    <ul>
+      {LIST.map((list, i) => (
+        <li key={i}>{list.text}</li>
       ))}
     </ul>
+    <table>
+      <thead>
+        {DATA[0].map((th, i) => (
+          <th key={i}>{th}</th>
+        ))}
+      </thead>
+      <tbody>
+        {DATA.filter((_, i) => i !== 0).map((row, i) => (
+          <tr key={i}>
+            {row.map((td, i) => (
+              <td key={i}>{td}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </StrictMode>
 );
